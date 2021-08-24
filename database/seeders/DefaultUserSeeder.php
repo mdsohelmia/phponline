e80e637d967f6ccf4e64d42a0204fa8f4f55f6f2
Steve McDougall <juststevemcd@gmail.com>
2021-08-24 20:06:57 +0100

    User data modelling done

---
+++ database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DefaultUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'first_name' => 'Steve',
+            'last_name' => 'McDougall',
+            'email' => 'juststevemcd@gmail.com',
+        ]);
+    }
+}
